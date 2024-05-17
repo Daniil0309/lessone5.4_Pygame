51bde77f32914a0db143141747e29c3ac44f7591
Daniil0309 <168443708+Daniil0309@users.noreply.github.com>
2024-05-17 14:29:03 +0300

    new basa

---
+++ main.py
@@ -0,0 +1,19 @@
+#Это самая база которая пригодится игры, приложения, итд.
+import pygame
+pygame.init()
+
+window_size = (800, 600)
+screen = pygame.display.set_mode(window_size)
+pygame.display.set_caption("Моя игра")
+
+run = True
+
+while run:
+    for event in pygame.event.get(): #event - событие, get - получить, каждое событие сохраняется в event переменной
+        if event.type == pygame.QUIT: #если тип события равен закрытию окна, крестик в окне
+            run = False
+
+    screen.fill((0, 0, 0)) #заполняем экран цветом
+    pygame.display.flip() #обновляем экран
+
+pygame.quit()
